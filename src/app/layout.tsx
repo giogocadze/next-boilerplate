640083e07076d8d269a196f5902e83ae492aa923
giogocadze <giogocadze5@gmail.com>
2026-01-31 11:26:24 +0400

    chore: use geist fonts and fix Container naming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Container from '@/components/Containert';
@@ -23,7 +24,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         <Container>{children}</Container>
         <Footer />
